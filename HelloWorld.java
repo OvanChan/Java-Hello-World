370c0dfe7a393f15f6a5e7c27476999ed3cf7d7d
OvanChan <ovanchan86@gmail.com>
2021-02-18 22:30:56 -0800

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+
+/*
+    To compile: javac HelloWorld.java
+    To run: java HelloWorld
+    Edited by: Ovan
+ */
+
+// declares a class hello world
+public class HelloWorld {
+
+    // "main" makes this class a java app that can be executed
+    public static void main(final String[] args) {
+
+        // output hello world to the system's output device
+        System.out.println("Hello world, hello Biden.");
+        
+    }
+
+}
+
